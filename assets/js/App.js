edcea4d8cd2e849f2d5c8f1a3b1afc53cfe01a1c
Wagner <wbrandaosoares@gmail.com>
2019-02-20 17:56:38 -0300

    continuacao das aulas

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
-import Routes from './routes';
+import Person from './components/Person/Person';
 
-import Header from './components/header';
-import Main from './pages/main'
+class App extends Component {
+    state = {
+        persons: [
+            {name: "Max", age: 28},
+            {name: "Manu", age: 26},
+            {name: "Stephanie", age: 29}
+        ]
+    }
 
-const App = () => (
-        <div className="App">
-            <Header/>
-            <Routes/>
-        </div>
-    );
+    switchNameHandler = () => {
+        this.setState({
+            persons: [
+                {name: "Maximilian", age: 28},
+                {name: "Manu", age: 26},
+                {name: "Stephanie", age: 27}
+            ]
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App!!</h1>
+                <p>This is really working!</p>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My work is done.</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+            </div>
+        );
+    }
+}
 
 export default App;

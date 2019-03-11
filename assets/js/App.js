5b730e1afa54167a89d34abaa26f1a6a00190d9b
Wagner <wbrandaosoares@gmail.com>
2019-03-11 15:11:43 -0300

    continuacao

--- assets/js/App.js
+++ assets/js/App.js
@@ -26,9 +26,16 @@ class App extends Component {
                 <h1>Hi, I'm a React App!!</h1>
                 <p>This is really working!</p>
                 <button onClick={this.switchNameHandler}>Switch Name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My work is done.</Person>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}/>
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler}>My work is done.</Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}/>
             </div>
         );
     }

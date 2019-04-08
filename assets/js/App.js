c9de8c71a95cc075c36cc87a4dfba996151beba1
Wagner <wbrandaosoares@gmail.com>
2019-04-08 16:21:38 -0300

    revisão

--- assets/js/App.js
+++ assets/js/App.js
@@ -10,11 +10,21 @@ class App extends Component {
         ]
     }
 
-    switchNameHandler = () => {
+    switchNameHandler = (name) => {
         this.setState({
             persons: [
-                {name: "Maximilian", age: 28},
-                {name: "Manu", age: 26},
+                {name: name, age: 28},
+                {name: "Manu", age: 29},
+                {name: "Stephanie", age: 27}
+            ]
+        });
+    }
+
+    nameChangeHandler = (event) => {
+        this.setState({
+            persons: [
+                {name: "Maxi", age: 28},
+                {name: event.target.value, age: 26},
                 {name: "Stephanie", age: 27}
             ]
         });
@@ -25,14 +35,15 @@ class App extends Component {
             <div className="App">
                 <h1>Hi, I'm a React App!!</h1>
                 <p>This is really working!</p>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <button onClick={() => this.switchNameHandler("Maximilian!!")}>Switch Name</button>
                 <Person
                     name={this.state.persons[0].name}
                     age={this.state.persons[0].age}/>
                 <Person
                     name={this.state.persons[1].name}
                     age={this.state.persons[1].age}
-                    click={this.switchNameHandler}>My work is done.</Person>
+                    click={this.switchNameHandler.bind(this, "Max!!")}
+                    changed={this.nameChangeHandler}>My work is done.</Person>
                 <Person
                     name={this.state.persons[2].name}
                     age={this.state.persons[2].age}/>
